310b803f7c734663b167525d8354badb43140787
PauRmz04 <59927209+PauRmz04@users.noreply.github.com>
2020-01-22 07:05:47 -0600

    Add files via upload

--- Ramirez_Paulina_P0.R
+++ Ramirez_Paulina_P0.R
@@ -4,24 +4,52 @@ Práctica 0/ Tarea 1
 ##1.- A partir de las siguientes secuencias concatenadas de RNA que te envío como archivo adjunto en formato fasta encuentra (usando librerías especializadas) la secuencia correspondiente de aminoácidos.
 
 library(Biostrings)
-readRNAStringSet("first.fasta")
+AAString("first.fasta")
 dna1<-readRNAStringSet("first.fasta")
 translate(dna1)
+
+
+
+
 
 ##2.- Escoge dos problemas de la plataforma Rosalind de entre los siguientes: Counting DNA Nucleotides, Complementing a Strand of DNA, Computing GC Content, Calculating Protein Mass. Escribe un programa que resuelva los dos problemas que seleccionaste, en ambos, casos, debes buscar una solución sin usar librerías especializadas y otra con librerías especializadas.
 
 #1Counting DNA Nucleotides
-
-dna2<- DNAString("AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC")
+phage2<- DNAString("CCGACCTCAGCGCAAGATGCTGTAACGAAGTCATATGCTGATGCGCTAACTAGCGGCCTTGATGTTAAGTCGAGCTGTAAAGTTGCGACGACTGCCAACATTACTTTGTCTGGCACTCAGACGATTGATGGTGTTGCAGTTTCTGCCGATGAGCGTGTGCTGGTCAAAAACCAGTCAACTGCTTCAGAGAACGGCATTTACGACTGTAAGTCTGGCAGCTGGGCACGTTCCAGTGACTTTGACAGCGATGCAGAAGTAACTTCTGGAGCGTTTACGTTTGTCGAGCAAGGCAGTGTCAACGCTGATGCTGGTTTTGTCCTGACGACTGATGGCAGCATCACTGTTGGCACAACTGCACTGAGCTTTACTCAGTTCTCTGGTGCTGGCGCAGTTACTGCTGGTGATGGCCTG")
+length(phage2)
 
 #Calcula la frecuecnia de cada uno de los nucleótidos de dichas secuencias
-alphabetFrequency(secuen, as.prob = TRUE)
+#Versión con los paquetes
+alphabetFrequency(phage2)
+
+#Nos indica la frecuencia de los nucleotidos presentes 
+
+#Versión sin paquete
+cadena1<-("CCGACCTCAGCGCAAGATGCTGTAACGAAGTCATATGCTGATGCGCTAACTAGCGGCCTTGATGTTAAGTCGAGCTGTAAAGTTGCGACGACTGCCAACATTACTTTGTCTGGCACTCAGACGATTGATGGTGTTGCAGTTTCTGCCGATGAGCGTGTGCTGGTCAAAAACCAGTCAACTGCTTCAGAGAACGGCATTTACGACTGTAAGTCTGGCAGCTGGGCACGTTCCAGTGACTTTGACAGCGATGCAGAAGTAACTTCTGGAGCGTTTACGTTTGTCGAGCAAGGCAGTGTCAACGCTGATGCTGGTTTTGTCCTGACGACTGATGGCAGCATCACTGTTGGCACAACTGCACTGAGCTTTACTCAGTTCTCTGGTGCTGGCGCAGTTACTGCTGGTGATGGCCTG")
+secuencia1 <- strsplit(cadena1, "")
+secuencia1 <- unlist(secuencia1)
+tabla1 <- table(secuencia1)
+tabla1
+
+#Para este lo que se trata de hacer una separación de la cadena, por lo cual de esta manera se encuentran la cantidad de elementos por separado. Finalmente en una tabla se acomodan los valores con las repeticiones para cada elemento.
+
+#2Computing GC Content
+
+phages3<- DNAStringSet(c("TGATCGAGACGTTTTAACCGTAGACCCAAGCAATTTTGTTTACTCCCAAGCAAGTGGTGAACGCCTCCGC", "ATCGCTTACGCTGACTTGACCGAATCACAGGTCATCGAATGGGTCAAAACCGCTCTTGGCGGTGACGAAA", "GTCGTTCAATTCTGGATGACGCTGATGCAGCCACTGTTCGTGCAACGCTTGGCCTTGTCATTGGAACGGA", "GTCAGTGACTGGATGATCCCATGGATGCACGACGTAATACACATGCAGCAGGATGGTGCGTACCATGCAG"))
+phages<- c("Phage1", "Phage2", "Phage3", "Phage4")
+names(phages3) <- phages
+
+#Contenido de GCas
+#Versión con paquete
+letterFrequency(phages3, "GC")
+
+#Esta nos indica la frecuencia de las GC presentes en las secuencias con  las que se está trabajando
 
 
-#2Complementing a Strand of DNA
-dna3<- DNAString("AAAACCCGGT")
-
-#Reverso complemento, nos proporciona la secuencia complementaria de la secuencia dada.
-reverseComplement(dna3)
-
+#Versión sin paquete
+cadena2<-("TGATCGAGACGTTTTAACCGTAGACCCAAGCAATTTTGTTTACTCCCAAGCAAGTGGTGAACGCCTCCGC")
+secuencia2 <- strsplit(cadena2, "")
+secuencia2 <- unlist(secuencia2)
+tabla2 <- table(secuencia2)
+tabla2
 
